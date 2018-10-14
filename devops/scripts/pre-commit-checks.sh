6ece64cdacc1e2a1801737809e5bd81411c3f017
Kelvin Wahome <kevowahome@gmail.com>
2018-10-14 22:47:51 +0300

    travis ci run test with docker

--- devops/scripts/pre-commit-checks.sh
+++ devops/scripts/pre-commit-checks.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 GITARGS=${1:-'diff --cached --name-only'}
-git $GITARGS | grep -E '.py$' | xargs flake8 ''
-[ $? -ne 0 ] && printf "\n\n\e[31m========= COMMIT REJECTED due to above listed style violations. ==========\e[0m\n\n" && exit 1
+if ! git "$GITARGS" | grep -E '.py$' | xargs black --check --exclude '((static|venv)/|/tests?(\.py$)?)'; then
+  printf '\n\n\e[31m========= COMMIT REJECTED due to above listed style violations. ==========\e[0m\n\n' && exit 1
+fi
+
+#GITARGS=${1:-'diff --cached --name-only'}
+#git $GITARGS | grep -E '.py$' | xargs flake8 ''
+#[ $? -ne 0 ] && printf "\n\n\e[31m========= COMMIT REJECTED due to above listed style violations. ==========\e[0m\n\n" && exit 1
 exit 0
--- devops/scripts/run_test.sh
+++ devops/scripts/run_test.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-docker-compose run web -e CC_TEST_REPORTER_ID=$CC_TEST_REPORTER_ID python manage.py test
+set -e
+
+# download codeclimate test coverage reporter program
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 >/usr/local/bin/cc-test-reporter &&
+  chmod +x /usr/local/bin/cc-test-reporter
+
+pip install black && ./devops/scripts/pre-commit-checks.sh "diff-tree --no-commit-id --name-only -r HEAD"
+cc-test-reporter before-build
+coverage erase
+./devops/docker/wait-for-it.sh db:5432
+coverage run --omit="*virtualenvs/*,*settings*,*manage*,*migrations*,*__init__*,*admin*,*celery*,*docs/*," manage.py test ||
+  exit 1
+bash <(curl -s https://codecov.io/bash)
+
+# if test coverage is less than 85%, the following command will exit with status code 2; thus failing the build
+coverage report --fail-under=80 || COVERAGE_REPORT_FAILED="yes"
+[[ $COVERAGE_REPORT_FAILED == "yes" ]] && printf '\n\n test code coverage is below required 80 percent. \n\n' && exit 2
+
+coverage xml
+cc-test-reporter after-build --exit-code $?
